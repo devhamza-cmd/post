c47847c1311a02b2bd1832c208f196f73c23edd3
devhamza-cmd <80796556+devhamza-cmd@users.noreply.github.com>
2023-01-25 13:58:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function checkLogin() {
       alert("Incorrect login credentials or empty fields. Please try again.");
     }
   }
+  
   document.getElementById("publish").addEventListener("click", function() {
     var firstDiv = document.querySelector(".first");
     var newDiv = document.createElement("div");
     newDiv.classList.add("firstpost");
     newDiv.style.marginTop = "40px";
-    newDiv.style.width = "100%";
-    newDiv.style.height = "25%";
+    newDiv.style.width = "70%";
+    newDiv.style.height = "150px";
     newDiv.style.boxShadow = "3px 3px 10px 3px #dddddd";
     newDiv.style.borderRadius = "20px";
     newDiv.style.padding = "20px";
@@ -41,8 +42,28 @@ function checkLogin() {
     newDiv.appendChild(qDiv);
   
     firstDiv.appendChild(newDiv);
-  
-    // Save the post to localStorage
+
+        var reactDiv = document.createElement("div");
+        reactDiv.classList.add("react", "d-flex", "mt-3", "justify-content-center", "justify-content-around");
+
+        var likeLink = document.createElement("a");
+        likeLink.href = "";
+        likeLink.innerHTML = '<i class="fa-solid fa-thumbs-up"></i>  Like';
+        reactDiv.appendChild(likeLink);
+    
+        var commentLink = document.createElement("a");
+        commentLink.href = "";
+        commentLink.innerHTML = '<i class="fa-solid fa-message"></i>  Comment';
+        reactDiv.appendChild(commentLink);
+    
+        var shareLink = document.createElement("a");
+        shareLink.href = "";
+        shareLink.innerHTML = '<i class="fa-solid fa-share"></i>  Share';
+        reactDiv.appendChild(shareLink);
+
+        newDiv.appendChild(reactDiv);
+    
+
     var posts = JSON.parse(localStorage.getItem("p")) || [];
     posts.push({
       text: inputText,
@@ -52,8 +73,6 @@ function checkLogin() {
   });
   window.addEventListener("load", function() {
     var firstDiv = document.querySelector(".first");
-  
-    // Retrieve the saved posts from localStorage
     var posts = JSON.parse(localStorage.getItem("p")) || [];
   
     for (var i = 0; i < posts.length; i++) {
@@ -61,8 +80,8 @@ function checkLogin() {
       var newDiv = document.createElement("div");
       newDiv.classList.add("firstpost");
       newDiv.style.marginTop = "40px";
-      newDiv.style.width = "100%";
-      newDiv.style.height = "25%";
+      newDiv.style.width = "70%";
+      newDiv.style.height = "150px";
       newDiv.style.boxShadow = "3px 3px 10px 3px #dddddd";
       newDiv.style.borderRadius = "20px";
       newDiv.style.padding = "20px";
@@ -90,4 +109,6 @@ function checkLogin() {
       firstDiv.appendChild(newDiv);
     }
   });
+
     
+  
